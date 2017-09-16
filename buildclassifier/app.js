0ff54a3e2f012b69bef728f04ae2e237d26f3540
RohitBandaru <rb696@cornell.edu>
2017-09-16 09:42:42 -0400

    started python setup

--- buildclassifier/app.js
+++ buildclassifier/app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -42,5 +43,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+var data=require('./routes/data')(app);
 
 module.exports = app;
---
+++ buildclassifier/routes/data.js
@@ -0,0 +1,35 @@
+/*
+Take data and send to python for compution and retrieve results
+
+*/
+var fs = require('fs');
+
+module.exports = function(app){
+	//http://www.sohamkamani.com/blog/2015/08/21/python-nodejs-comm/
+	fs.readFile('public/data/data.json', (err, data) => {  
+	    if (err) throw err;
+	    let parsedData = JSON.parse(data);
+	    //console.log(parsedData);
+
+		//start.js
+		var spawn = require('child_process').spawn
+		var py = spawn('python3', ['./python/data_test.py'])
+		var count = 0
+		var dataString = ""
+		var count = 0
+		py.stdout.on('data', function(data){
+			console.log(data)
+		  	dataString += data.toString();
+		});
+		
+		py.stdout.on('end', function(){
+			console.log(dataString);
+		});
+		//send JSON to python as string
+		py.stdin.write(JSON.stringify(parsedData));
+
+		py.stdin.end();
+
+	}); 
+
+}
