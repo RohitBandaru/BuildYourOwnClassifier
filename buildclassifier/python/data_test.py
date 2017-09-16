62b75be37857bc94620e2318a03e07b75ffec9a8
RohitBandaru <rb696@cornell.edu>
2017-09-16 12:21:32 -0400

    updated data.json

--- buildclassifier/python/data_test.py
+++ buildclassifier/python/data_test.py
@@ -1,18 +1,17 @@
 ## compute_input.py
 
 import sys, json, numpy as np
-
 #Read data from stdin
 def read_in():
     lines = sys.stdin.readlines()
     #Since our input would only be having one line, parse our JSON data from that
-    return (lines[0])
+    return json.loads(lines[0])
 
 def main():
     #get our data as an array from read_in()
     lines = read_in()
+    print(json.dumps(lines))
 
-    print(lines)
 
 #start process
 if __name__ == '__main__':
--- buildclassifier/routes/data.js
+++ buildclassifier/routes/data.js
@@ -8,25 +8,26 @@ module.exports = function(app){
 	//http://www.sohamkamani.com/blog/2015/08/21/python-nodejs-comm/
 	fs.readFile('public/data/data.json', (err, data) => {  
 	    if (err) throw err;
-	    let parsedData = JSON.parse(data);
-	    //console.log(parsedData);
+	    let parsedData = JSON.parse(data)["data"];
 
-		//start.js
 		var spawn = require('child_process').spawn
 		var py = spawn('python3', ['./python/data_test.py'])
+
 		var count = 0
 		var dataString = ""
 		var count = 0
+
 		py.stdout.on('data', function(data){
-			console.log(data)
-		  	dataString += data.toString();
+			dataString += data
 		});
 		
 		py.stdout.on('end', function(){
-			console.log(dataString);
+			response = JSON.parse(dataString);
+			console.log(response);
 		});
+
 		//send JSON to python as string
-		py.stdin.write(JSON.stringify(parsedData));
+		py.stdin.write(JSON.stringify(data));
 
 		py.stdin.end();
 
